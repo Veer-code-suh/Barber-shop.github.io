3c03ce682645063c97f1c5f3b708ddea738e6c3a
VIRENDRA MAURYA <mauryaveer4050@gmail.com>
2023-06-10 20:29:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+sideNav.style.right = "-250px";
+
+menuBtn.onclick = function() {
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0";
+        menu.src = "close.png";
+    } else {
+        sideNav.style.right = "-250px";
+        menu.src = "menu.png";
+    }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 300,
+    speedAsDuration: true
+});
